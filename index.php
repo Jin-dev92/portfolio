257bce9a54c0b33db56dcd4ba1dfa7cd7a430185
Jin-devST <31203142+Jin-dev92@users.noreply.github.com>
2020-07-10 13:48:30 +0900

    dd

--- index.php
+++ index.php
@@ -3,18 +3,6 @@
 ?>
 
 <body>
-    <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient">
-        <div class="container"><a class="navbar-brand logo" href="./index.html">JinDev</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse"
-                id="navbarNav">
-                <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item" role="presentation"><a class="nav-link active" href="./index.html">Home</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="./projects.html">Projects</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="./contact.html">Contact</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
     <a href="./"></a>
     <main class="page lanidng-page">
         <section class="portfolio-block block-intro">
