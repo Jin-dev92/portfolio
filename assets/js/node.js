57ce264bfe9b50b1d5afdea32f377f760f09c728
Jin-devST <31203142+Jin-dev92@users.noreply.github.com>
2020-07-10 13:46:55 +0900

    aa

---
+++ assets/js/node.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var url = require('url');
+var http = require('http');
+// var nowUrl = document.location.href;
+
+// console.log(nowUrl);
+
+var querydata = url.parse(nowUrl,true).query;
+// fs.readFile(`../../project/${}`)
+// function createProject(title) {
+    
+// }
+
+// function createList(fileList) {
+    
+// }
+
--- index.php
+++ index.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - JinDev</title>
-    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-</head>
+<?php 
+    include_once('./head.php');
+?>
 
 <body>
     <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient">
